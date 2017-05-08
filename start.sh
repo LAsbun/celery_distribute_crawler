8d25e32c07a602bece88fbd8b3b8c393e66f817f
LAsbun <794866972@qq.com>
2017-05-08 15:47:49 +0800

    启动脚本测试

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-cd celery_distribute_crawler; nohup python -m flower > ../flower.log &
+cd celery_distribute_crawler; nohup stdbuf -oL python -m flower | cronolog ../flower.log &
+echo "xx"
 cd ..;
 nohup stdbuf -oL celery -A celery_distribute_crawler.celery0 worker -l info -P gevent -c 500 |cronolog ~/celery_log/%Y%m%d/%Y%m%d%H/celery.log.%Y%m%d%H &
 nohup celery -A celery_distribute_crawler.celery0 beat -l info > beat.log &
