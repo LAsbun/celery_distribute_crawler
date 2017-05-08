e2dd44c7166ba69f02c5f5baad181a3d6c349191
LAsbun <794866972@qq.com>
2017-05-08 16:50:33 +0800

    调整并发数

--- worker_restart.sh
+++ worker_restart.sh
@@ -2,4 +2,5 @@
 ps aux|grep "celery -A celery_distribute_crawler.celery0 worker"|grep -v grep|tee -a worker_restart.log
 ps aux|grep "celery -A celery_distribute_crawler.celery0 worker"|grep -v grep|cut -c 10-15|xargs kill -9
 echo "restart_all done"
+cd ~/dis; . bin/activate;
 sh celery_distribute_crawler/worker_start.sh
