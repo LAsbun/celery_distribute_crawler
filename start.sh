70dd2005c8f599e366ee291bcf0ab354efdac183
LAsbun <794866972@qq.com>
2017-05-10 12:06:54 +0800

    调整并发数

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ nohup stdbuf -oL python -m flower | cronolog ../flower.log &
 echo "xx"
 cd ..;
 nohup stdbuf -oL python -m celery_distribute_crawler.server > /dev/null &
-nohup stdbuf -oL celery -A celery_distribute_crawler.celery0 worker -l info -P gevent -c 50 |cronolog ~/celery_log/%Y%m%d/%Y%m%d%H/celery.log.%Y%m%d%H &
+nohup stdbuf -oL celery -A celery_distribute_crawler.celery0 worker -l info -P gevent |cronolog ~/celery_log/%Y%m%d/%Y%m%d%H/celery.log.%Y%m%d%H &
 nohup celery -A celery_distribute_crawler.celery0 beat -l info > beat.log &
