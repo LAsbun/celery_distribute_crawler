72f03888a9e118bbbda46a462043fdd59448db26
LAsbun <794866972@qq.com>
2017-05-24 21:47:17 +0800

    模板配置好了

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ echo "xx"
 cd ..;
 nohup stdbuf -oL python -m celery_distribute_crawler.server > /dev/null &
 nohup stdbuf -oL celery -A celery_distribute_crawler.celery0 worker -l info -P gevent |cronolog ~/celery_log/%Y%m%d/%Y%m%d%H/celery.log.%Y%m%d%H &
-nohup celery -A celery_distribute_crawler.celery0 beat -l info > beat.log &
+#nohup celery -A celery_distribute_crawler.celery0 beat -l info > beat.log &
