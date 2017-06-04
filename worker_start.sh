e04bcd76c8449709b51443df12994eb4a35c84c6
LAsbun <794866972@qq.com>
2017-06-04 13:52:57 +0800

    设置eventlet

--- worker_start.sh
+++ worker_start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nohup stdbuf -oL celery -A celery_distribute_crawler.celery0 worker -l info |cronolog ~/celery_log/%Y%m%d/%Y%m%d%H/celery.log.%Y%m%d%H &
+nohup stdbuf -oL celery -A celery_distribute_crawler.celery0 worker -l info -P eventlet |cronolog ~/celery_log/%Y%m%d/%Y%m%d%H/celery.log.%Y%m%d%H &
